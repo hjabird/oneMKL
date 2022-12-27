f4ec8e57ad5129244e2f6f84dcc19e9fde8230ad
Hugh Bird <hugh.bird@codeplay.com>
2022-12-27 15:17:23 +0000

    Use same default strides for real and complex domains

--- src/dft/descriptor.cxx
+++ src/dft/descriptor.cxx
@@ -27,40 +27,10 @@ namespace mkl {
 namespace dft {
 namespace detail {
 
-// Compute the default strides for a domain. Modifies real_strides and complex_strides
-// argument.
-template <domain dom>
-static void compute_default_strides(const std::vector<std::int64_t>& dimensions, int rank,
-                                    std::vector<std::int64_t>& input_strides,
-                                    std::vector<std::int64_t>& output_strides);
-
-template <>
-void compute_default_strides<domain::REAL>(const std::vector<std::int64_t>& dimensions, int rank,
-                                           std::vector<std::int64_t>& input_strides,
-                                           std::vector<std::int64_t>& output_strides) {
-    // Compute strides for CCE format.
-    // See MKL C interface developer reference for explanation of the below.
-    std::vector<std::int64_t> strides(rank + 1, 1);
-    // The first variable stride value is different.
-    if (rank > 1) {
-        strides[rank - 1] = dimensions[rank - 1] / 2 + 1;
-    }
-    for (int i = rank - 2; i > 0; --i) {
-        strides[i] = strides[i + 1] * dimensions[i];
-    }
-    strides[0] = 0;
-    output_strides = strides;
-    for (int i = 1; i < rank; ++i) {
-        strides[i] *= 2;
-    }
-    input_strides = std::move(strides);
-}
-
-template <>
-void compute_default_strides<domain::COMPLEX>(const std::vector<std::int64_t>& dimensions, int rank,
-                                              std::vector<std::int64_t>& input_strides,
-                                              std::vector<std::int64_t>& output_strides) {
-    // Default is for COMPLEX_COMPLEX storage
+// Compute the default strides. Modifies real_strides and complex_strides arguments.
+void compute_default_strides(const std::vector<std::int64_t>& dimensions, int rank,
+                             std::vector<std::int64_t>& input_strides,
+                             std::vector<std::int64_t>& output_strides) {
     std::vector<std::int64_t> strides(rank + 1, 1);
     for (int i = rank - 1; i > 0; --i) {
         strides[i] = strides[i + 1] * dimensions[i];
@@ -166,7 +136,7 @@ descriptor<prec, dom>::descriptor(std::vector<std::int64_t> dimensions)
         }
     }
     // Assume forward transform.
-    compute_default_strides<dom>(dimensions_, rank_, values_.input_strides, values_.output_strides);
+    compute_default_strides(dimensions_, rank_, values_.input_strides, values_.output_strides);
     values_.bwd_scale = 1.0;
     values_.fwd_scale = 1.0;
     values_.number_of_transforms = 1;
